0ae6c0580a5b29ec3c7ba13c9b2bddcf0b25f645
kappasao <alvarosalomoni.97@gmail.com>
2016-11-11 19:56:05 +0100

    Ejercicio11

---
+++ src/Ejercicio11.java
@@ -0,0 +1,11 @@
+/**
+ * Created by 46995932D on 11/11/2016.
+ */
+public class Ejercicio11 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 65; i <= 90; i++) {
+            System.out.println((char) i);
+        }
+    }
+}
+
